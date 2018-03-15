8d429e10084b395a7c17c355496411e26a5fbe2f
Saar Raz <saarraz1@gmail.com>
2018-03-15 05:08:43 +0200

    EmitTopLevelDecl modification (present in Phabricator but not here).

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -4064,6 +4064,7 @@ void CodeGenModule::EmitTopLevelDecl(Decl *D) {
   case Decl::UsingShadow:
   case Decl::ClassTemplate:
   case Decl::VarTemplate:
+  case Decl::Concept:
   case Decl::VarTemplatePartialSpecialization:
   case Decl::FunctionTemplate:
   case Decl::TypeAliasTemplate:
